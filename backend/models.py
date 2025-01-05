943c67bbdde2b7f4027c68f01d788662a41cddfe
Venya <venyadurgam25@gmail.com>
2025-01-05 21:57:12 +0530

    Updated models.py file

--- backend/models.py
+++ backend/models.py
@@ -7,3 +7,14 @@ class Friend(db.Model):
     description = db.Column(db.Text,nullable=False)
     gender = db.Column(db.String(10),nullable=False)
     img_url = db.Column(db.String(200),nullable=True)
+
+    def to_json(self):
+        return {
+            "id":self.id,
+            "name":self.name,
+            "role":self.role,
+            "description":self.description,
+            "gender":self.gender,
+            "imgUrl":self.img_url,
+
+        }
