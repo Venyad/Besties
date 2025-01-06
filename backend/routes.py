9085f6d577958b0b7d449094e7cb7cf85099da11
Venya <venyadurgam25@gmail.com>
2025-01-06 22:36:31 +0530

    Updated routes.py file

--- backend/routes.py
+++ backend/routes.py
@@ -1,31 +1,33 @@
-from app import app,db
-from flask import request,jsonify
+from app import app, db
+from flask import request, jsonify
 from models import Friend
 
-#Get all friends
+# Get all friends
 
-@app.route("/api/friends",methods=["GET"])
+
+@app.route("/api/friends", methods=["GET"])
 def get_friends():
     friends = Friend.query.all()
-    result =  [friend.to_json() for friend in friends]
+    result = [friend.to_json() for friend in friends]
     return jsonify(result)
 
-#Create a friend
+# Create a friend
 
-@app.route("/api/friends",methods=["POST"])
+
+@app.route("/api/friends", methods=["POST"])
 def create_friend():
     try:
         data = request.json
-        required_fields = ["name","role","description","gender"]
+        required_fields = ["name", "role", "description", "gender"]
         for field in required_fields:
             if field not in data:
-                return jsonify({"error":f'Missing required field:{field}'}),400
+                return jsonify({"error": f'Missing required field:{field}'}), 400
         name = data.get("name")
         role = data.get("role")
         description = data.get("description")
         gender = data.get("gender")
 
-        #Fetch avatar image based on gender
+        # Fetch avatar image based on gender
         if gender == "male":
             img_url = f"https://avatar.iran.liara.run/public/boy?username={name}"
         elif gender == "female":
@@ -33,13 +35,30 @@ def create_friend():
         else:
             img_url = None
 
-        new_friend = Friend(name=name, role=role,description=description,gender=gender,img_url=img_url)
+        new_friend = Friend(
+            name=name, role=role, description=description, gender=gender, img_url=img_url)
         db.session.add(new_friend)
 
         db.session.commit()
 
-        return jsonify({"msg":"Friend created successfully"}),201
-    
+        return jsonify({"msg": "Friend created successfully"}), 201
+
+    except Exception as e:
+        db.session.rollback()
+        return jsonify({"error": str(e)}), 500
+
+# Delete a friend
+
+
+@app.route("/api/friends/<int:id>", methods=["DELETE"])
+def delete_friend(id):
+    try:
+        friend = Friend.query.get(id)
+        if friend is None:
+            return jsonify({"error":"Friend not found"}),404
+        db.session.delete(friend)
+        db.session.commit()
+        return jsonify({"msg":"Friend deleted"}),200
     except Exception as e:
         db.session.rollback()
         return jsonify({"error":str(e)}),500
