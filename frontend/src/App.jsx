56b379a7ea748b4dbb453f67e2a6f83e341ff817
Venya <venyadurgam25@gmail.com>
2025-01-07 17:16:07 +0530

    Added frontend files

---
+++ frontend/src/App.jsx
@@ -0,0 +1,14 @@
+import { Button,Box } from "@chakra-ui/react"
+function App() {
+
+  return (
+    <>
+      <Box p={5} color="blue.500">
+        This is a Chakra UI Box
+      </Box>
+      <Button colorScheme="blue">Hello</Button>
+    </>
+  )
+}
+
+export default App
