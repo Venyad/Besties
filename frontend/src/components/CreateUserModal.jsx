0d8b4bd9e350c150a5b10078b1bbcf34ea8ee06f
Venya <venyadurgam25@gmail.com>
2025-01-08 19:24:57 +0530

    Updated Create Use Modal file

--- frontend/src/components/CreateUserModal.jsx
+++ frontend/src/components/CreateUserModal.jsx
@@ -1,11 +1,23 @@
-import React from 'react'
-
+import { Button, useDisclosure } from "@chakra-ui/react";
+import { BiAddToQueue } from "react-icons/bi";
 const CreateUserModal = () => {
-  return (
-    <div>
-      
-    </div>
-  )
+  const { isOpen, onOpen, onClose } = useDisclosure()
+  return(
+  <>
+    <Button>
+      <BiAddToQueue size={20} />
+    </Button>
+    <Modal
+    isOpen={isOpen}
+    onClose = {onClose}
+
+    >
+    <ModalOverlay/>
+    <ModalContent>
+      <ModalHeader>My new BFF </ModalHeader>
+    </ModalContent>
+    </Modal>
+  </>)
 }
 
 export default CreateUserModal
