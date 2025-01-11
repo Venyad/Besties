89965dd1b524546ff896689eee8f0de455834452
Venya <venyadurgam25@gmail.com>
2025-01-11 21:59:21 +0530

    Updated usercard component

--- frontend/src/components/UserCard.jsx
+++ frontend/src/components/UserCard.jsx
@@ -1,11 +1,31 @@
-import React from 'react'
+import { Card, CardHeader, Flex,Avatar,Box,Heading } from "@chakra-ui/react"
+import {BiTrash} from 'react-icons/bi'
+const UserCard = ({user}) => {
+    return (
+        <Card>
+            <CardHeader>
+                <Flex gap={4}>
+                    <Flex flex={"1"} gap={"4"} alignItems={"center"}><Avatar src={user.imgUrl} />
 
-const UserCard = () => {
-  return (
-    <div>
-      
-    </div>
-  )
+                        <Box>
+                            <Heading size='sm'>{user.name}</Heading>
+                            <Text>{user.role}</Text>
+                        </Box></Flex>
+                    <Flex>
+                        {/* <EditModal ></EditModal> */}
+                        <IconButton
+							variant='ghost'
+							colorScheme='red'
+							size={"sm"}
+							aria-label='See menu'
+							icon={<BiTrash size={20} />}
+							onClick={handleDeleteUser}
+						/>
+                    </Flex>
+                </Flex>
+            </CardHeader>
+        </Card>
+    )
 }
 
 export default UserCard
