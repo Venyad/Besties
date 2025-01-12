6754f932c2da720ada14294d1d14bffe219f91dc
Venya <venyadurgam25@gmail.com>
2025-01-12 22:33:22 +0530

    Updated user grid file

--- frontend/src/components/UserGrid.jsx
+++ frontend/src/components/UserGrid.jsx
@@ -1,9 +1,27 @@
 import { Grid } from "@chakra-ui/react"
 import UserCard from "./UserCard"
-import { USERS } from "@/dummy/dummy"
 import { useState } from "react"
 
-const UserGrid = ({users}) => {
+const UserGrid = ({users,setUsers}) => {
+  const [isLoading, setIsLoading] = useState(true);
+  useEffect(() => {
+		const getUsers = async () => {
+			try {
+				const res = await fetch("http://127.0.0.1:5000/api/friends");
+				const data = await res.json();
+
+				if (!res.ok) {
+					throw new Error(data.error);
+				}
+				setUsers(data);
+			} catch (error) {
+				console.error(error);
+			} finally {
+				setIsLoading(false);
+			}
+		};
+		getUsers();
+	}, [setUsers]);
   return (
     <Grid templateColumns={{
       base:"1fr",
