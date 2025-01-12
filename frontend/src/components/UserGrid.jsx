87036c3225ff4c41d09e8264a257d30cb45416b4
Venya <venyadurgam25@gmail.com>
2025-01-12 22:35:03 +0530

    Updated user grid file

--- frontend/src/components/UserGrid.jsx
+++ frontend/src/components/UserGrid.jsx
@@ -2,39 +2,56 @@ import { Grid } from "@chakra-ui/react"
 import UserCard from "./UserCard"
 import { useState } from "react"
 
-const UserGrid = ({users,setUsers}) => {
+const UserGrid = ({ users, setUsers }) => {
   const [isLoading, setIsLoading] = useState(true);
   useEffect(() => {
-		const getUsers = async () => {
-			try {
-				const res = await fetch("http://127.0.0.1:5000/api/friends");
-				const data = await res.json();
+    const getUsers = async () => {
+      try {
+        const res = await fetch("http://127.0.0.1:5000/api/friends");
+        const data = await res.json();
 
-				if (!res.ok) {
-					throw new Error(data.error);
-				}
-				setUsers(data);
-			} catch (error) {
-				console.error(error);
-			} finally {
-				setIsLoading(false);
-			}
-		};
-		getUsers();
-	}, [setUsers]);
+        if (!res.ok) {
+          throw new Error(data.error);
+        }
+        setUsers(data);
+      } catch (error) {
+        console.error(error);
+      } finally {
+        setIsLoading(false);
+      }
+    };
+    getUsers();
+  }, [setUsers]);
   return (
-    <Grid templateColumns={{
-      base:"1fr",
-      md: "repeat(2, 1fr)",
-					lg: "repeat(3, 1fr)",
-				
-    }}
-    gap={4}>
-      {users.map((user) => (
-					<UserCard key={user.id} user={user} />
-				))}
+    <>
+      <Grid templateColumns={{
+        base: "1fr",
+        md: "repeat(2, 1fr)",
+        lg: "repeat(3, 1fr)",
 
-    </Grid>
+      }}
+        gap={4}>
+        {users.map((user) => (
+          <UserCard key={user.id} user={user} />
+        ))}
+
+      </Grid>
+      {isLoading && (
+				<Flex justifyContent={"center"}>
+					<Spinner size={"xl"} />
+				</Flex>
+			)}
+			{!isLoading && users.length === 0 && (
+				<Flex justifyContent={"center"}>
+					<Text fontSize={"xl"}>
+						<Text as={"span"} fontSize={"2xl"} fontWeight={"bold"} mr={2}>
+							Poor you! 🥺
+						</Text>
+						No friends found.
+					</Text>
+				</Flex>
+			)}
+    </>
   )
 }
 
