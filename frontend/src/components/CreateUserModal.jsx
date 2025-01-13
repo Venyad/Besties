0bc7152f887cf1fcf54bb1dadb4e44bb1e61c7da
Venya <venyadurgam25@gmail.com>
2025-01-13 22:15:56 +0530

    Updated create user modal file

--- frontend/src/components/CreateUserModal.jsx
+++ frontend/src/components/CreateUserModal.jsx
@@ -1,57 +1,81 @@
-import { Button, useDisclosure, ModalOverlay,Flex,
-        FormControl,Input,Modal,ModalBody,ModalCloseButton,ModalContent,ModalFooter,ModalHeader,Radio,RadioGroup,Textarea } from "@chakra-ui/react";
+import {
+  Button, useDisclosure, ModalOverlay, Flex,
+  FormControl, Input, Modal, ModalBody, ModalCloseButton, ModalContent, ModalFooter, ModalHeader, Radio, RadioGroup, Textarea, useDisclosure, useToast
+} from "@chakra-ui/react";
+import { useState } from "react";
 import { BiAddToQueue } from "react-icons/bi";
-const CreateUserModal = () => {
+const CreateUserModal = ({ setUsers }) => {
   const { isOpen, onOpen, onClose } = useDisclosure()
-  return(
-  <>
-    <Button>
-      <BiAddToQueue size={20} />
-    </Button>
-    <Modal
-    isOpen={isOpen}
-    onClose = {onClose}
+  const [isLoading, setIsLoading] = useState(false);
+  const [inputs, setInputs] = useState({
+    name: "",
+    role: "",
+    description: "",
+    gender: "",
+  });
+  const toast = useToast();
 
-    >
-    <ModalOverlay/>
-    <ModalContent>
-      <ModalHeader>My new BFF 🔥 </ModalHeader>
-      <ModalCloseButton/>
-      <ModalBody pb={6}>
-        <Flex alignItems={"center"} gap={4}>
-          <FormControl>
-            <FormLabel>Full Name</FormLabel>
-            <Input placeholder="John Doe"/>
 
-          </FormControl>
-          <FormControl>
-            <FormLabel>Role</FormLabel>
-            <Input placeholder="Software Engineer"/>
+  return (
+    <>
+      <Button>
+        <BiAddToQueue size={20} />
+      </Button>
+      <Modal
+        isOpen={isOpen}
+        onClose={onClose}
 
-          </FormControl>
-        </Flex>
-        <FormControl mt={4}>
-            <FormLabel>Description</FormLabel>
-            <Textarea resize={"none"} overflowY={"hidden"} placeholder="He's a software engineer who loves to code and build things."></Textarea>
+      >
+        <form onSubmit={handleCreateUser}>
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader>My new BFF 🔥 </ModalHeader>
+          <ModalCloseButton />
+          <ModalBody pb={6}>
+            <Flex alignItems={"center"} gap={4}>
+              <FormControl>
+                <FormLabel>Full Name</FormLabel>
+                <Input placeholder="John Doe"
+                  value={inputs.name}
+                  onChange={(e) => setInputs({ ...inputs, name: e.target.value })} />
 
-          </FormControl>
-          <RadioGroup mt={4}>
-            <Flex gap={5}>
-              <Radio value='male'>Male</Radio>
-              <Radio value='female'>Female</Radio>
+              </FormControl>
+              <FormControl>
+                <FormLabel>Role</FormLabel>
+                <Input placeholder="Software Engineer"
+                  value={inputs.role}
+                  onChange={(e) => setInputs({ ...inputs, role: e.target.value })} />
 
+              </FormControl>
             </Flex>
-          </RadioGroup>
-      </ModalBody>
-      <ModalFooter>
-        <Button colorScheme={blue} mr={3}>
-          Add
-        </Button>
-        <Button onClick={onClose}>Cancel</Button>
-      </ModalFooter>
-    </ModalContent>
-    </Modal>
-  </>)
+            <FormControl mt={4}>
+              <FormLabel>Description</FormLabel>
+              <Textarea resize={"none"} overflowY={"hidden"} placeholder="He's a software engineer who loves to code and build things."
+                value={inputs.description}
+                onChange={(e) => setInputs({ ...inputs, description: e.target.value })}
+              ></Textarea>
+
+            </FormControl>
+            <RadioGroup mt={4}>
+              <Flex gap={5}>
+                <Radio value='male'
+                  onChange={(e) => setInputs({ ...inputs, gender: e.target.value })}>Male</Radio>
+                <Radio value='female'
+                  onChange={(e) => setInputs({ ...inputs, gender: e.target.value })}>Female</Radio>
+
+              </Flex>
+            </RadioGroup>
+          </ModalBody>
+          <ModalFooter>
+            <Button colorScheme={blue} mr={3} type='submit' isLoading={isLoading}>
+              Add
+            </Button>
+            <Button onClick={onClose}>Cancel</Button>
+          </ModalFooter>
+        </ModalContent>
+        </form>
+      </Modal>
+    </>)
 }
 
 export default CreateUserModal
